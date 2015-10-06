c4808c95d61d449c5c04cf0ea1d4e517a2917952
Angel-Avila <angel.armando.96@hotmail.com>
2015-10-06 16:52:59 -0700

    Añadí la clase de partículas

---
+++ Project/src/juego/entity/particle/Particle.java
@@ -0,0 +1,11 @@
+package src.juego.entity.particle;
+
+import java.util.List;
+
+import src.juego.entity.Entity;
+
+public class Particle extends Entity{
+
+	private List<Particle> particles = new ArrayList<Particle>;
+	
+}
